f6ae9c73ebb18feee324a4dc0a0a41cbfdea2eff
Mathieu Trudelle <73758838+trudelle200902@users.noreply.github.com>
2025-01-13 10:02:41 -0500

    read two temp sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define TMP_SENSOR_EXT A0
+
+#define DHTPIN 2 // Pin where the DHT sensor is connected
+#define DHTTYPE DHT11 // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  dht.begin();
+  pinMode(TMP_SENSOR_EXT, INPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  delay(100);
+  uint8_t val_ext = analogRead(TMP_SENSOR_EXT);
+  float_t temp_ext = (val_ext-0.5)*100;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  float_t humidity = dht.readHumidity();
+  float_t temperature_int = dht.readTemperature();
+
+  Serial.print("Temperature eau: ");
+  Serial.print(temperature_int);
+  Serial.print("exterieur: ");
+  Serial.print(temp_ext);
 }
